c669b98e3a03465ec6eb6cb1de711a5c6a7a0bf1
pranav k <pranavkkeloth@gmail.com>
2023-10-12 17:45:42 +0530

    feat: Implement attribute directives [ngStyle]&&[ngClass]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,58 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'firstApp';
   isDisabled: boolean = true;
   msgFlag: boolean = true;
-  f : boolean = true;
+  f: boolean = true;
+  superPower = 'angular';
+  violet = 'violet';
+  textColor = 'red';
+  tax: Number = 10;
+
+  getClass(){
+    return 'c3';
+  }
   contacts = [
-    {'firstName': 'pkyou',
-    'lastName': 'Tutorials',
-    'contactId':1234
-    },
+    { firstName: 'pkyou', lastName: 'Tutorials', contactId: 1234 },
     {
-      'firstName':'Mark',
-      'lastName':'Hender',
-      'contactId':67895
+      firstName: 'Mark',
+      lastName: 'Hender',
+      contactId: 67895,
     },
-    {'firstName': 'pkyou',
-    'lastName': 'Tutorials',
-    'contactId':1234
-    },
+    { firstName: 'pkyou', lastName: 'Tutorials', contactId: 1234 },
     {
-      'firstName':'Mark',
-      'lastName':'Hender',
-      'contactId':67895
+      firstName: 'Mark',
+      lastName: 'Hender',
+      contactId: 67895,
     },
-    {'firstName': 'pkyou',
-    'lastName': 'Tutorials',
-    'contactId':1234
-    },
+    { firstName: 'pkyou', lastName: 'Tutorials', contactId: 1234 },
     {
-      'firstName':'Mark',
-      'lastName':'Hender',
-      'contactId':67895
+      firstName: 'Mark',
+      lastName: 'Hender',
+      contactId: 67895,
     },
-    {'firstName': 'pkyou',
-    'lastName': 'Tutorials',
-    'contactId':1234
-    },
+    { firstName: 'pkyou', lastName: 'Tutorials', contactId: 1234 },
     {
-      'firstName':'Mark',
-      'lastName':'Hender',
-      'contactId':67895
+      firstName: 'Mark',
+      lastName: 'Hender',
+      contactId: 67895,
     },
-    {'firstName': 'pkyou',
-    'lastName': 'Tutorials',
-    'contactId':1234
-    },
+    { firstName: 'pkyou', lastName: 'Tutorials', contactId: 1234 },
     {
-      'firstName':'Mark',
-      'lastName':'Hender',
-      'contactId':67895
-    }
-  ]
+      firstName: 'Mark',
+      lastName: 'Hender',
+      contactId: 67895,
+    },
+  ];
 }
