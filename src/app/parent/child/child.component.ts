ed1f508248c2065d42cda9ab47246359c952db93
pranav k <pranavkkeloth@gmail.com>
2023-10-26 13:58:28 +0530

    feat: Implement attribute and structural directives

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -9,7 +9,7 @@ export class ChildComponent implements OnInit{
 
   numbers : any = [1, 2, 3, 4, 5];
   oddNumbers : any = [1,3,5]
-  evenNumbers : any = [2,4]
+  evenNumbers : any = [2,4,6]
   onlyOdd = true;
 
   childData : string = "Data from child component" 
