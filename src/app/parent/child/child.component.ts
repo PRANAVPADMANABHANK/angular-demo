04ae76ac1c91ffbcd18eb6d0d433ccb93456dd38
pranav k <pranavkkeloth@gmail.com>
2023-10-26 12:41:37 +0530

    feat: Implement structural directives

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -7,6 +7,11 @@ import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class ChildComponent implements OnInit{
 
+  numbers : any = [1, 2, 3, 4, 5];
+  oddNumbers : any = [1,3,5]
+  evenNumbers : any = [2,4]
+  onlyOdd = true;
+
   childData : string = "Data from child component" 
 
   @Output() childStringData : EventEmitter<string> = new EventEmitter();
@@ -24,5 +29,9 @@ export class ChildComponent implements OnInit{
 
   SendData(){
     this.childStringData.emit(this.childData)
+  }
+
+  onChange(){
+
   }
 }
